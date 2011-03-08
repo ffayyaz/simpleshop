eb58df45a3f549bdd3a9d187a018c632a1d11429
fafa <fafa@hermes.(none)>
2011-03-08 13:43:24 +0100

    new simple shop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleshop::Application.config.secret_token = '0eac42ae2b0a840f6b3ab897f569a5f0eefcc5e1784631b951f44b43c0f776a14e32ce7093e83b50b08eba9efce65c021c4f5213a9d4e3882d97138bcbfc3ad3'
